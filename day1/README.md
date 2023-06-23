4bcd2155017790a440be48217e10ee1f2ecc90b4
mrblackhearts <greenthumb2284@gmail.com>
2023-06-23 14:22:28 +0500

    Commit message

--- day1/README.md
+++ day1/README.md
@@ -1,4 +1,4 @@
-# Day 1 - Working with Variables in Python to Manage Data
+# Section 1: Day 1 - Working with Variables in Python to Manage Data
 ## Concepts Practised
 - Printing to the Console in Python
 - String Manipulation and Code Intelligence
@@ -9,4 +9,4 @@
 ## Band Name Generator
 ![day01](https://user-images.githubusercontent.com/98851253/154177081-2c53df2d-777b-4deb-8e38-5742ecd7282f.gif)
 
-https://replit.com/@phillipra/band-name-generator-start
+https://replit.com/@TalhaAsghar3/band-name-generator-start
--- day1/main.py
+++ day1/main.py
@@ -1,4 +1,9 @@
-print("Welcome to The Band Name Generator!")
-city = input("What city did you grow up in?\n")
-pet = input("What is the name of your first pet?\n")
-print("Your band name is " + city +  " " + pet)
+print("Welcome To Band Name Generator")
+# Asking the user for the city that they grew up in.
+city_name = input("In which city did you grow up in : ")
+# Asking the user for the name of a pet.
+pet_name = input("Write the name of a pet  : ")
+# Creating band name
+band_name = city_name + " " + pet_name
+print(band_name)
+
